a9bb140b4343dee87e97c46eac8eeb7256aaf307
Snehal <snehalwagh2121@gmail.com>
2023-03-29 09:43:55 +0530

    SPRING-38 - Gateway service changes

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
 @Slf4j
+@RequestMapping("/service1")
 public class SampleController {
 
     @Value("${message}")
