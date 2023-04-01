d63fdafb90711247a25d459039e551b4728ce70c
Snehal <snehalwagh2121@gmail.com>
2023-04-01 22:42:46 +0530

    SPRING-41 - Zipkin sleuth integration service1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +14,8 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RefreshScope
@@ -16,10 +18,23 @@ public class SampleController {
     @Value("${message}")
     String defaultMsg;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("/getmessage")
     public String loadMessage() {
         log.info("checking runtime config values: " + defaultMsg);
         return this.defaultMsg;
+    }
+
+    @GetMapping("/getmessagefromservice2")
+    public String loadMessageFromService2() {
+        log.info("calling service 2 /message api");
+        String msg= restTemplate.getForObject(
+                "http://localhost:9011/service2/message/",
+                String.class
+        );
+        return msg;
     }
 
 }
